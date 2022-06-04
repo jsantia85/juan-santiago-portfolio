569502f0eaf76e7eac3309530441da77e0898fa9
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-04 18:47:12 -0400

    sets up event listner for aboutBtn

--- js/app.js
+++ js/app.js
@@ -7,11 +7,18 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+//create cached el ref for about button
+const aboutBtn = document.querySelector("#aboutBtn")
+// console.log(aboutBtn)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+//create event listener for about btn
+aboutBtn.addEventListener('click', handleClick)
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function handleClick () {
+  console.log('About button has been clicked!')
+}
