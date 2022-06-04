25832599984e194df995bee54aec9e36c1ae4e56
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-04 19:55:39 -0400

    links buttons to href

--- js/app.js
+++ js/app.js
@@ -7,18 +7,9 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-//create cached el ref for about button
-const aboutBtn = document.querySelector("#aboutBtn")
-// console.log(aboutBtn)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-//create event listener for about btn
-aboutBtn.addEventListener('click', handleClick)
 
 
 /*-------------------------------- Functions --------------------------------*/
-
-function handleClick () {
-  console.log('About button has been clicked!')
-}
