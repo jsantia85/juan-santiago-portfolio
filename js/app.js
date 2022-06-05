f21014ed78cb174742d967aa15ca0a4e77f9dd07
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-04 21:26:55 -0400

    adds event listener to btns

--- js/app.js
+++ js/app.js
@@ -7,9 +7,19 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
+const btns = document.getElementById('btns')
+console.log(btns)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+btns.addEventListener("mouseover", function( event ) {
+  // highlight the mouseover target
+  event.target.style.color = "#D9D065";
 
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
 
 /*-------------------------------- Functions --------------------------------*/
