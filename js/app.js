7bf33c907e8bf796c3b16ebb4fa6dd2ad1c0fc2c
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-06 11:18:17 -0400

    adds underline to about seciton on mouseover

--- js/app.js
+++ js/app.js
@@ -10,9 +10,11 @@
 const btns = document.getElementById('btns')
 // console.log(btns)
 
+const aboutSection = document.getElementById('aboutSection')
+console.log(aboutSection)
 /*----------------------------- Event Listeners -----------------------------*/
-btns.addEventListener("mouseover", function( event ) {
-  // highlight the mouseover target
+btns.addEventListener("mouseover", function(event) {
+  // highlight the mouseOver target
   event.target.style.color = "#a4161a";
 
   // reset the color after a short delay
@@ -21,7 +23,15 @@ btns.addEventListener("mouseover", function( event ) {
   }, 500);
 }, false);
 
+aboutSection.addEventListener("mouseover", function(event) {
+  //highlight the mouseOver target
+  event.target.style.textDecoration = "underline";
 
+  //reset underline after short delay
+  setTimeout(function() {
+    event.target.style.textDecoration = ""
+  }, 1000);
+}, false)
 /*-------------------------------- Functions --------------------------------*/
 function fadeInTitle() {
   const titleEl = document.querySelector(".title");
