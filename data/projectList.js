4efa1e76aa42905cd7ac50c32ef5b363b4e27322
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-06 16:11:31 -0400

    creates array of object for external js file

---
+++ data/projectList.js
@@ -0,0 +1,9 @@
+const projectList = [
+  { 
+    image: "imgs/Tic-Tac-Toe.jpg.png",
+    title: "Tic-Tac-Toe",
+    description: "Interactive Game",
+    gitHubLink: "https://github.com/jsantia85/ttt-weekend.git",
+    deploymentLink: "https://ttt-jsantia-977211.netlify.app/",
+  }
+]
