fb86477e08642751f2d1bc274543266a97114e8f
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-08 17:49:45 -0400

    changes description for tictactoe

--- js/projects.js
+++ js/projects.js
@@ -4,7 +4,7 @@ let projects = [
     deployment: "https://ttt-jsantia-977211.netlify.app/",
     image: "/assets/imgs/Tic-Tac-Toe.jpg.png",
     gitHub: "https://github.com/jsantia85/ttt-weekend.git",
-    description: "Interactive Game"
+    description: "A Classic Game!"
   }
 ]
 
