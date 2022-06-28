0a2e65b3c46c255aefb1fe04833933fc7dd3eda3
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-28 14:05:27 -0400

    adds projects

--- js/projects.js
+++ js/projects.js
@@ -4,14 +4,28 @@ let projects = [
     deployment: "https://ttt-jsantia-977211.netlify.app/",
     image: "/assets/imgs/Tic-Tac-Toe.jpg.png",
     gitHub: "https://github.com/jsantia85/ttt-weekend.git",
-    description: "A Classic Game!"
+    description: "Two player game, built by JavaScript."
   },
   { 
     title: "Portfolio",
     deployment: "https://juan-santiago-portfolio-99.netlify.app/",
     image: "assets/imgs/Portfolio.png",
     gitHub: "https://github.com/jsantia85/juan-santiago-portfolio",
-    description: "Personal Site"
+    description: "Personal site built using, HTML, CSS, and JavaScript."
+  },
+  { 
+    title: "Mongoose Flights",
+    deployment: "https://mongoose-flights-js.herokuapp.com/",
+    image: "assets/imgs/Mongoose Flights.png",
+    gitHub: "https://github.com/jsantia85/mongoose-flights",
+    description: "Flight tracker built using MEN stack."
+  },
+  { 
+    title: "MoPower",
+    deployment: "https://juan-santiago-portfolio-99.netlify.app/",
+    image: "assets/imgs/MoPower-Landing-Page.png",
+    gitHub: "https://github.com/jsantia85/mo-power",
+    description: "MoPower is a build tracker using MEN stack for car enthusiast!"
   }
 ]
 
