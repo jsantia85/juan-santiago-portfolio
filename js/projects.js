3df5665f0fe22ec9fe27464b47109e6793137077
JASantia85 <JuanASantiago1058@gmail.com>
2022-07-20 22:05:04 -0400

    changes layout of projects

--- js/projects.js
+++ js/projects.js
@@ -1,24 +1,10 @@
 let projects = [
   { 
-    title: "Tic-Tac-Toe",
-    deployment: "https://ttt-jsantia-977211.netlify.app/",
-    image: "assets/imgs/Tic-Tac-Toe.png",
-    gitHub: "https://github.com/jsantia85/ttt-weekend.git",
-    description: "Two player game, built by JavaScript."
-  },
-  { 
-    title: "Portfolio",
-    deployment: "https://juan-santiago-portfolio-99.netlify.app/",
-    image: "assets/imgs/Portfolio-Landing.png",
-    gitHub: "https://github.com/jsantia85/juan-santiago-portfolio",
-    description: "Personal site built using, HTML, CSS, and JavaScript."
-  },
-  { 
-    title: "Mongoose Flights",
-    deployment: "https://mongoose-flights-js.herokuapp.com/",
-    image: "assets/imgs/Mongoose Flights.png",
-    gitHub: "https://github.com/jsantia85/mongoose-flights",
-    description: "Flight tracker built using MEN stack."
+    title: "Code-Ipedia",
+    deployment: "https://code-ipedia.netlify.app/",
+    image: "assets/imgs/Code-Ipedia.png",
+    gitHub: "https://github.com/jsantia85/code-ipedia-front-end",
+    description: "Code-Ipedia is a MERN Stack CRUD application for saving and sharing code!"
   },
   { 
     title: "MoPower!",
@@ -28,11 +14,25 @@ let projects = [
     description: "MoPower is a build tracker using MEN stack for car enthusiast!"
   },
   { 
-    title: "Code-Ipedia",
-    deployment: "https://code-ipedia.netlify.app/",
-    image: "assets/imgs/Code-Ipedia.png",
-    gitHub: "https://github.com/jsantia85/code-ipedia-front-end",
-    description: "Code-Ipedia is a MERN Stack CRUD application for saving and sharing code!"
+    title: "Mongoose Flights",
+    deployment: "https://mongoose-flights-js.herokuapp.com/",
+    image: "assets/imgs/Mongoose Flights.png",
+    gitHub: "https://github.com/jsantia85/mongoose-flights",
+    description: "Flight tracker built using MEN stack."
+  },
+  { 
+    title: "Portfolio",
+    deployment: "https://juan-santiago-portfolio-99.netlify.app/",
+    image: "assets/imgs/Portfolio-Landing.png",
+    gitHub: "https://github.com/jsantia85/juan-santiago-portfolio",
+    description: "Personal site built using, HTML, CSS, and JavaScript."
+  },
+  { 
+    title: "Tic-Tac-Toe",
+    deployment: "https://ttt-jsantia-977211.netlify.app/",
+    image: "assets/imgs/Tic-Tac-Toe.png",
+    gitHub: "https://github.com/jsantia85/ttt-weekend.git",
+    description: "Two player game, built by JavaScript."
   }
 ]
 
